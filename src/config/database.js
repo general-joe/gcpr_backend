7be32bd24a6e487ee203b9e07d63acca4a9cc610
Amankona Joseph <kwabijoe274@gmail.com>
2026-01-30 09:12:51 +0000

    Project set up

---
+++ src/config/database.js
@@ -0,0 +1,35 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+// Create a PostgreSQL pool
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Create the Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// Shared Prisma client instance
+const globalForPrisma = globalThis;
+
+/** @type {import('@prisma/client').PrismaClient} */
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ["warn", "error"], // optional
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+export default prisma;
