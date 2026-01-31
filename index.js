1a6eb676660e92594dd3d55beae96d879d28acf8
samuelfreeman <samuelmanueljnr@gmail.com>
2026-01-31 10:40:48 +0000

    feat:completed server

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict'
+
+import dotenv from 'dotenv'
+import app from './src/server.js'
+import cron from 'node-cron'
+// import CronService from './server/services/cron/CronService.js'
+dotenv.config()
+
+// Run every minute to catch exact opening times
+// cron.schedule('* * * * *', CronService.reset_Opening_Time)
+
+// //  other cron jobs
+// cron.schedule('* * * * *', CronService.clearOldOrders)
+// cron.schedule('0 23 * * *', CronService.processScheduledWithdrawals)
+// cron.schedule('* * * * *', CronService.clearOldPromotions)
+
+const port = process.env.PORT || 3000
+
+
+export default app.listen(port, () => {
+    WRITE.info(`Server is started at : ${port} `)
+})
+
---
+++ src/server.js
@@ -0,0 +1,65 @@
+import dotenv from 'dotenv'
+import compression from 'compression'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Global Variables
+import WRITE from './utils/logger.js'
+
+import CONSTANTS from './utils/constants.js'
+import MOMENT from 'moment'
+import _ from 'lodash'
+import gcprError from './utils/http-error.js'
+
+// ROUTING
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
+const app = express()
+
+
+global.WRITE = WRITE
+global.CONSTANTS = CONSTANTS
+global.MOMENT = MOMENT
+global._ = _
+global.gcprError = gcprError
+
+app.use(compression())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, './files')))
+
+app.get('/', (req, res) => {
+    res.send({
+        status: 'ok',
+        env: process.env.NODE_ENV,
+        date: MOMENT(),
+        visitor: req.ip,
+        version: 1.0
+    })
+})
+
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}))
+
+app.use(cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
+    credentials: true,
+    exposedHeaders: ['x-auth-token']
+}))
+
+app.use(bodyParser.json({
+    limit: '50mb',
+    extended: true
+}))
+
+
+
+export default app
