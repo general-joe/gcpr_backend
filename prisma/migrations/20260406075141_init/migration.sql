9ba5d4cc27c0ad27633b585a6141a8aed113f5ae
lockwud <oklement3@gmail.com>
2026-04-06 18:59:02 +0000

    feat: add caption column to CommunityMessage, CommunityAnnouncement, and directMessage tables

---
+++ prisma/migrations/20260406075141_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "CommunityMessage" ADD COLUMN "caption" TEXT;
+
+-- AlterTable
+ALTER TABLE "CommunityAnnouncement" ADD COLUMN "caption" TEXT;
+
+-- AlterTable
+ALTER TABLE "directMessage" ADD COLUMN "caption" TEXT;
