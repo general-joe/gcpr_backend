b225f543a817122165234c81cc9467aabc44a731
Samuel Boateng <samuelmanueljnr@gmail.com>
2026-05-11 00:49:29 +0000

    Log user request data in registerUser method

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -55,6 +55,7 @@ const buildIdentifierWhere = (identifier) => {
 
 class AuthService {
   static async registerUser(rq, userData) {
+    WRITE.info("User request data", { userData });
     if (rq.files?.profileImage) {
       const fileName = `${userData.id}.jpg`;
       userData.profileImage = await UploadService.saveFile(
