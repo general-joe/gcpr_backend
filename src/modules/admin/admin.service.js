f705d701a32d6b5899f03cb32f7a814d6e364ab5
lockwud <oklement3@gmail.com>
2026-08-16 07:25:41 +0000

    Fix: Allow multiple user deletions by not modifying phoneNumber on soft delete

--- src/modules/admin/admin.service.js
+++ src/modules/admin/admin.service.js
@@ -170,7 +170,6 @@ class AdminService {
       data: {
         accountStatus: "DELETED",
         email: { set: null },
-        phoneNumber: { set: "DELETED_ACCOUNT" },
         password: { set: "DELETED_ACCOUNT" },
       },
     });
